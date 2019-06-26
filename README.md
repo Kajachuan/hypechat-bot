04adbc81549bdc1c19eece89dba383a3543fb19b
Kajachuan <kevincajachuan@hotmail.com>
2019-06-26 17:34:08 -0300

    Casi todo listo (falta mute)

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 * `@tito help`: muestra los comandos disponibles
 * `@tito info`: muestra información del canal: integrantes, cantidad de mensajes, etc
 * `@tito mute <n>`: desactiva respuestas por n segundos
-* `@tito me`: muestra información del usuario que envia el mensaje.
+* `@tito me`: muestra información del usuario que envía el mensaje.
 
 Además de estos comandos tito deberá poseer acciones automáticas:
 
-* Mensaje de bievenida: al ingresar un usuario nuevo se deberá generar un saludo de bienvenida.
+* Mensaje de bienvenida: al ingresar un usuario nuevo se deberá generar un saludo de bienvenida.
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+var request = require('requestify');
 const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hola. Soy el Bot Tito');
+})
+
+app.get('/help', function (req, res) {
+  res.json({help: '@tito help: muestra los comandos disponibles.',
+            info: '@tito info: muestra información del canal: integrantes, cantidad de mensajes, etc.',
+            mute: '@tito mute <n>: desactiva respuestas por n segundos.',
+            me:   '@tito me: muestra información del usuario que envía el mensaje.'});
+})
+
+app.get('/info', function (req, res) {
+  let org = req.query.org;
+  let msg;
+
+  request.get('http://127.0.0.1:8000/organization/' + org).then(function (response) {
+    let body = response.getBody()
+    res.json({name: body.name,
+              owner: body.owner,
+              description: body.description})
+  });
+})
+
+app.get('/me', function (req, res) {
+  let user = req.query.user;
+
+  request.get('http://127.0.0.1:8000/profile/' + user).then(function (response) {
+    let body = response.getBody()
+    res.json({username: user,
+              first_name: body.first_name,
+              last_name: body.last_name,
+              email: body.email})
+  });
 })
 
 app.listen(process.env.PORT || 3000);
