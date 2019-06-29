4a9ceb889b8a33faa519d3a3399b9d70158ef905
Kajachuan <kevincajachuan@hotmail.com>
2019-06-29 17:01:12 -0300

    Mute listo

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 var request = require('requestify');
+var cache = require('memory-cache');
 const express = require("express");
 const app = express();
+const server_url = process.env.PORT ? 'https://hypechat-taller2-staging.herokuapp.com/' : 'http://127.0.0.1:8000/';
+
+app.use(function (req, res, next) {
+  if(cache.get(req.query.user) == null) {
+    next();
+  } else {
+    res.json({message: 'No puedo responder porque fui muteado por ' + cache.get(req.query.user) + ' segundos'});
+  }
+});
 
 app.get('/', function (req, res) {
-  res.send('Hola. Soy el Bot Tito');
-})
+  res.json({message: 'Hola ' + req.query.user + '. Escribí "@tito help" para ver los comandos disponibles'});
+});
 
 app.get('/help', function (req, res) {
-  res.json({help: '@tito help: muestra los comandos disponibles.',
-            info: '@tito info: muestra información del canal: integrantes, cantidad de mensajes, etc.',
-            mute: '@tito mute <n>: desactiva respuestas por n segundos.',
-            me:   '@tito me: muestra información del usuario que envía el mensaje.'});
-})
+  res.json({help: '"@tito help": muestra los comandos disponibles.',
+            info: '"@tito info": muestra información del canal: integrantes, cantidad de mensajes, etc.',
+            mute: '"@tito mute <n>": desactiva respuestas por n segundos.',
+            me: '"@tito me": muestra información del usuario que envía el mensaje.'});
+});
 
 app.get('/info', function (req, res) {
-  let org = req.query.org;
-  let msg;
-
-  request.get('http://127.0.0.1:8000/organization/' + org).then(function (response) {
+  request.get(server_url + 'organization/' + req.query.org).then(function (response) {
     let body = response.getBody()
     res.json({name: body.name,
               owner: body.owner,
               description: body.description})
   });
-})
+});
 
 app.get('/me', function (req, res) {
-  let user = req.query.user;
-
-  request.get('http://127.0.0.1:8000/profile/' + user).then(function (response) {
+  request.get(server_url + 'profile/' + req.query.user).then(function (response) {
     let body = response.getBody()
     res.json({username: user,
               first_name: body.first_name,
               last_name: body.last_name,
               email: body.email})
   });
-})
+});
+
+app.get('/mute%20:seconds(\\d+)', function (req, res) {
+  cache.put(req.query.user, req.params.seconds, Number(req.params.seconds) * 1000);
+  res.json({message: 'Ahora estoy muteado por ' + req.params.seconds + ' segundos'});
+});
 
 app.listen(process.env.PORT || 3000);
